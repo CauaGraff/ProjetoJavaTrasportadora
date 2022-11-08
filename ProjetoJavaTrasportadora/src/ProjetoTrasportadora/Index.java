594e5e88ddbf8c76cfa82cf714d47f45faaade88
Cauã Graff <cauagraff@gmail.com>
2022-11-07 22:03:15 -0300

    criação do menu

---
+++ ProjetoJavaTrasportadora/src/ProjetoTrasportadora/Index.java
@@ -0,0 +1,49 @@
+package ProjetoTrasportadora;
+
+import java.util.Scanner;
+
+public class Index {
+
+	public static void main(String[] args) {
+			System.out.println("***** MENU PRINCIPAL *******");
+			Scanner scan = new Scanner(System.in);
+			System.out.println("1 Cadastros");
+			System.out.println("2 Relatorios");
+			System.out.println("3 Ajuda");
+			System.out.println("0 Sair");
+			System.out.print("Digite a operaçao desejada: ");
+			int op = scan.nextInt();
+	 
+ 
+			if (op == 1) {
+				menuCadastro();
+			} else if (op == 2) {
+//				menuRelatorio();
+			}
+			scan.close();
+		}
+		
+		
+		public static void menuCadastro() {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("***** MENU CADASTRO *******");
+			System.out.println("1 Funcionario");
+			System.out.println("2 Fornecedor");
+			System.out.println("3 Produos");
+			System.out.println("0 Voltar");
+			System.out.println("Digite a operaçao desejada: ");
+			int op = scan.nextInt();
+
+			if (op == 1) {
+//				irCadastroFuncionar();
+				 
+			} else if (op == 0) {
+			main(null);
+			}
+			
+			scan.close();
+		}
+
+	}
+
+
